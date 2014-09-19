0a49965d67a00535cfd6b0cabe928ed8279b6d94
Chi Wang <chiwang@mail.ustc.edu.cn>
2014-09-19 15:01:57 +0200

    modified:   src/DmpAlgBgoPed.cc

--- src/DmpAlgBgoPed.cc
+++ src/DmpAlgBgoPed.cc
@@ -27,7 +27,6 @@ DmpAlgBgoPed::DmpAlgBgoPed()
   fBgoPed(0)
 {
   gRootIOSvc->Set("OutData/Key","ped");
-  //gRootIOSvc->Set("OutData/FileName",gRootIOSvc->GetInputFileName());
 }
 
 //-------------------------------------------------------------------
